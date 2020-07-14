b5c4b8bfe16a5b7fea7b1b761e2f31a5c04db139
Erika Hentsch <erikahentsch@hotmail.com>
2020-07-14 13:04:29 -0400

    added frontend code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
 
-
+app.use('', express.static('public'));
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 app.get('/', (req,res)=> {
     res.send("Hello World")

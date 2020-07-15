2dbeb1839701c6f26b44f2a910673fa31a2a1016
Erika Hentsch <erikahentsch@hotmail.com>
2020-07-15 14:55:43 -0400

    added .env to project

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+require('dotenv').config();
 
 
 app.get('/', (req,res)=> {
-    res.send("Hello from the back end")
+    res.send("Hello from the back end "+process.env.test)
 })
 
 

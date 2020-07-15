7df3bfbeacd8b6b0c01a21b661c58ef26310967d
Erika Hentsch <erikahentsch@hotmail.com>
2020-07-15 15:28:50 -0400

    added api call to test

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path')
+const request = require('request')
+require('dotenv').config();
+
 const app = express();
 
 
@@ -8,11 +11,22 @@ app.get('/', (req,res)=> {
 })
 
 app.get('/env', (req,res)=> {
-    res.send(process.env.DEFAULT);
+    res.send(process.env);
     
 })
 
+app.get("/image/:id", async (req, res)=> {
+    let imgUrl = `http://www.vizzion.com/TrafficCamsService/TrafficCams.ashx?strRequest=GetCameraImage7&intCameraID=${req.params.id}&intDesiredWidth=720&intDesiredHeight=480&intDesiredDepth=8&intOptions=0&strPassword=ZUY%5b%5bBB%5cB3%5bWSVBBIJIQZHU%26IFEO`
+    request.get(imgUrl)
+        .pipe(res)
+})
 
+app.get("/imagetest/:id", async (req, res)=> {
+    // console.log(process.env.API_KEY)
+    let imgUrl = `http://www.vizzion.com/TrafficCamsService/TrafficCams.ashx?strRequest=GetCameraImage7&intCameraID=${req.params.id}&intDesiredWidth=720&intDesiredHeight=480&intDesiredDepth=8&intOptions=0&strPassword=${process.env.API_KEY}`
+    request.get(imgUrl)
+        .pipe(res)
+})
 
 const PORT = process.env.port || 8080
 
